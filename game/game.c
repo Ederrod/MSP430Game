c7ad5c9cef496227af0df87e8e940dbb9d46eae6
Eder Rodriguez <ederrod21@gmail.com>
2018-11-24 20:14:44 -0700

    working on custom shape

--- game/game.c
+++ game/game.c
@@ -3,6 +3,19 @@
 #include "lcddraw.h"
 #include "shape.h"
 
+AbRect rect5 = {abRectGetBounds, abRectCheck, {5,5}}; 
+
+
+Layer layer0 = {
+    (AbShape*)&rect5,
+    {screenWidth/2,screenHeight/2},
+    {0,0},
+    {0,0},
+    COLOR_GOLD,
+    0
+}; 
+
+
 int 
 main()
 {
@@ -12,4 +25,5 @@ main()
 
     clearScreen(COLOR_BLUE);
 
+    layerDraw(&layer0); 
 }
