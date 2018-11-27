96d4db717ac173905ccf06ee4f8cd647642ce656
Eder Rodriguez <ederrod21@gmail.com>
2018-11-26 21:05:04 -0700

    clean up

--- game/game.c
+++ game/game.c
@@ -41,88 +41,8 @@ Layer layer0 = {		/**< Layer with a red square */
   &fieldLayer,
 };
 
-/** Moving Layer
- *  Linked list of layer references
- *  Velocity represents one iteration of change (direction & magnitude)
- */
-typedef struct MovLayer_s {
-  Layer *layer;
-  Vec2 velocity;
-  struct MovLayer_s *next;
-} MovLayer;
-
 /* initial value of {0,0} will be overwritten */
 MovLayer ml0 = { &layer0, {2,2}, 0 }; 
-
-void movLayerDraw(MovLayer *movLayers, Layer *layers)
-{
-  int row, col;
-  MovLayer *movLayer;
-
-  and_sr(~8);			/**< disable interrupts (GIE off) */
-  for (movLayer = movLayers; movLayer; movLayer = movLayer->next) { /* for each moving layer */
-    Layer *l = movLayer->layer;
-    l->posLast = l->pos;
-    l->pos = l->posNext;
-  }
-  or_sr(8);			/**< disable interrupts (GIE on) */
-
-
-  for (movLayer = movLayers; movLayer; movLayer = movLayer->next) { /* for each moving layer */
-    Region bounds;
-    layerGetBounds(movLayer->layer, &bounds);
-    lcd_setArea(bounds.topLeft.axes[0], bounds.topLeft.axes[1], 
-		bounds.botRight.axes[0], bounds.botRight.axes[1]);
-    for (row = bounds.topLeft.axes[1]; row <= bounds.botRight.axes[1]; row++) {
-      for (col = bounds.topLeft.axes[0]; col <= bounds.botRight.axes[0]; col++) {
-        Vec2 pixelPos = {col, row};
-        u_int color = bgColor;
-        Layer *probeLayer;
-        for (probeLayer = layers; probeLayer; 
-            probeLayer = probeLayer->next) { /* probe all layers, in order */
-          if (abShapeCheck(probeLayer->abShape, &probeLayer->pos, &pixelPos)) {
-            color = probeLayer->color;
-            break; 
-          } /* if probe check */
-        } // for checking all layers at col, row
-        lcd_writeColor(color); 
-      } // for col
-    } // for row
-  } // for moving layer being updated
-}	  
-
-
-
-//Region fence = {{10,30}, {SHORT_EDGE_PIXELS-10, LONG_EDGE_PIXELS-10}}; /**< Create a fence region */
-
-/** Advances a moving shape within a fence
- *  
- *  \param ml The moving shape to be advanced
- *  \param fence The region which will serve as a boundary for ml
- */
-void mlAdvance(MovLayer *ml, Region *fence)
-{
-  Vec2 newPos;
-  u_char axis;
-  Region shapeBoundary;
-  for (; ml; ml = ml->next) {
-    vec2Sub(&newPos, &ml->layer->posNext, &ml->velocity);
-    abShapeGetBounds(ml->layer->abShape, &newPos, &shapeBoundary);
-    for (axis = 0; axis < 2; axis++) {
-      if ((shapeBoundary.topLeft.axes[axis] > fence->topLeft.axes[axis]) ||
-        (shapeBoundary.botRight.axes[axis] < fence->botRight.axes[axis]) ) {
-	int velocity = ml->velocity.axes[axis] = 0;
-        newPos.axes[axis] -= (2*velocity);
-      }
-      /*elseif((shapeBoundary.topLeft.axes[axis] > 0)||(shapeBoundary.botRight.axes[axis]< 0)){
-	int vel = ml->velocity.axes[axis] = 0;
-	newPos.axes[axis] -= (2*vel);
-      }/**< if outside of fence */
-    } /**< for axis */
-    ml->layer->posNext = newPos;
-  } /**< for ml */
-}
-
 
 u_int bgColor = COLOR_BLUE;     /**< The background color */
 int redrawScreen = 1;           /**< Boolean for whether screen needs to be redrawn */
