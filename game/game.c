dee0ee6079d4bcbd3ad85ca20a6b2b5830b1e1f9
Rodriguez <eirodriguez5@miners.utep.edu>
2018-11-30 10:50:16 -0700

    adding game state engine

--- game/game.c
+++ game/game.c
@@ -7,67 +7,8 @@
  *  is turned off along with the green LED.
  */  
 #include <msp430.h>
-#include <libTimer.h>
-#include <lcdutils.h>
-#include <lcddraw.h>
-#include <p2switches.h>
-#include <shape.h>
-#include <abCircle.h>
 
-#include "mov_layer.h"
-
-#define GREEN_LED BIT6
-
-#define SIZE 8
-
-AbRect rect10 = {abRectGetBounds, abRectCheck, {SIZE, SIZE/2}};
-
-AbRectOutline fieldOutline = {	/* playing field */
-  abRectOutlineGetBounds, abRectOutlineCheck,   
-  {screenWidth/2, screenHeight/2}
-};
-
-Layer fieldLayer = {		/* playing field as a layer */
-  (AbShape *) &fieldOutline,
-  {screenWidth/2, screenHeight/2},/**< center */
-  {0,0}, {0,0},				    /* last & next pos */
-  COLOR_BLACK,
-  0
-};
-
-Layer layer0 = {		/**< Layer with a red square */
-  (AbShape *)&rect10,
-  {screenWidth/2, screenHeight-SIZE/2}, /**< center */
-  {0,0}, {0,0},				    /* last & next pos */
-  COLOR_RED,
-  &fieldLayer,
-};
-
-Layer layer1 = {
-  (AbShape *)&circle6,
-  {SIZE, SIZE}, /**< bit below & right of center */
-  {0,0}, {0,0},				    /* last & next pos */
-  COLOR_ORANGE,
-  0
-};
-
-Layer layer2 = {
-  (AbShape *)&circle4,
-  {SIZE+SIZE, SIZE}, /**< bit below & right of center */
-  {0,0}, {0,0},				    /* last & next pos */
-  COLOR_ORANGE,
-  0
-};
-/* initial value of {0,0} will be overwritten */
-MovLayer ml0 = { &layer0, {1,0}, 0 };
-
-
-MovLayer ml1 = { &layer1, {0,-2}, 0 }; 
-
-u_int bgColor = COLOR_BLUE;     /**< The background color */
-int redrawScreen = 1;           /**< Boolean for whether screen needs to be redrawn */
-
-Region fieldFence;		/**< fence around playing field  */
+#include "game_engine.h"
 
 
 /** Initializes everything, enables interrupts and green LED, 
@@ -75,89 +16,19 @@ Region fieldFence;		/**< fence around playing field  */
  */
 void main()
 {
-  P1DIR |= GREEN_LED;		/**< Green led on when CPU on */		
-  P1OUT |= GREEN_LED;
-
-  configureClocks();
-  lcd_init();
-  shapeInit();
-  p2sw_init(15);
-
-  shapeInit();
-
-  layerInit(&layer0);
-  layerDraw(&layer0);
-
-  layerInit(&layer1);
-  layerDraw(&layer1);
-
-  layerInit(&layer2);
-  layerDraw(&layer2);
-
-  layerGetBounds(&fieldLayer, &fieldFence);
-
-  enableWDTInterrupts();      /**< enable periodic interrupt */
-  or_sr(0x8);	              /**< GIE (enable interrupts) */
-
-  for(;;) { 
-    while (!redrawScreen) { /**< Pause CPU if screen doesn't need updating */
-      P1OUT &= ~GREEN_LED;    /**< Green led off witHo CPU */
-      or_sr(0x10);	      /**< CPU OFF */
-    }
-    P1OUT |= GREEN_LED;       /**< Green led on when CPU on */
-    redrawScreen = 0;
-    movLayerDraw(&ml0, &layer0);
-    movLayerDraw(&ml1, &layer1); 
-  }
+  game_init(); 
 }
 
 /** Watchdog timer interrupt handler. 15 interrupts/sec */
 void wdt_c_handler()
 {
-  short enemies = screenWidth/SIZE; 
   static short count = 0;
   P1OUT |= GREEN_LED;		      /**< Green LED on when cpu on */
   count ++;
-
-  MovLayer asteroids; 
-  short i; 
-
-  for (i = 0; i < 1; i++){
-    int num = (rand() %  (2 - 1 + 1)) + 1;
-    Layer temp =  (num == 1) ? layer1 : layer2;
-    temp.pos = {0, (rand() % ((screenWidth-8)-SIZE + 1) +SIZE}; 
-    asteroids.layer = &temp; 
-    asteroids.velocity = {0,-2}; 
-    asteroids.next = 0; 
+  if (game_state){
+    count = game_run(count); 
+  }else {
+    game_end(); 
   }
-
-  u_int switches = p2sw_read();
-
-  if((switches & (1<<0)) == 0){
-    mlPlayerAdvanceLeft(&ml0, &fieldFence);
-    redrawScreen = 1;  
-  }
-
-  if((switches & (1<<3)) == 0){
-    mlPlayerAdvanceRight(&ml0, &fieldFence);
-    redrawScreen = 1;  
-  }
-
-  if (count == 15) {
-    if (p2sw_read())
-      redrawScreen = 1;
-    //count = 0;
-  } 
-
-  if (count == 30){
-    mlAsteroidAdvance(&asteroids, &fieldFence); 
-    char end = collisionDetection(&asteroids, &ml0);
-    if (end)
-      drawString5x7(40, 70, "You Suck", COLOR_GOLD, bgColor); 
-    if (p2sw_read())
-      redrawScreen = 1;
-    count = 0;
-  }
-
   P1OUT &= ~GREEN_LED;		    /**< Green LED off when cpu off */
 }
---
+++ game/game_engine.h
@@ -0,0 +1,6 @@
+#define GREEN_LED BIT6
+#define SIZE 8
+
+void game_init(); 
+short game_run(short counter); 
+void game_end(); 
